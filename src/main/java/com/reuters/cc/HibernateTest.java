c42a6aa50a8a90c5626a6764ac4a1f609cb7b1c2
kiwichengcheng <kiwichengcheng@163.com>
2014-11-14 17:51:35 +0800

    add properties

--- src/main/java/com/reuters/cc/HibernateTest.java
+++ src/main/java/com/reuters/cc/HibernateTest.java
@@ -1,5 +1,7 @@
 package com.reuters.cc;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.context.ApplicationContext;
@@ -14,16 +16,33 @@ public class HibernateTest {
 		// TODO Auto-generated method stub
 
 		UserDetails user = new UserDetails();
-		user.setUserId(1);
 		user.setUserName("First User");
+		user.setAddress("First User's address");
+		user.setJoinedDate(new Date());
+		user.setDescription("Description of the user goes here");
+		
+		UserDetails user2 = new UserDetails();
+		user2.setUserName("Second User");
+		user2.setAddress("First User's address");
+		user2.setJoinedDate(new Date());
+		user2.setDescription("Description of the user goes here");
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		SessionFactory sessionFactory = context.getBean("sessionFactory",SessionFactory.class);
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(user);
+		session.save(user2);
 		session.getTransaction().commit();
 		
+		/*session.close();
+		
+		user = null;
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		user = (UserDetails)session.get(UserDetails.class, 1);
+		
+		System.out.println("User Name retrieved is "+user.getUserName());*/
 	}
 
 }
--- src/main/java/com/reuters/cc/model/UserDetails.java
+++ src/main/java/com/reuters/cc/model/UserDetails.java
@@ -1,20 +1,53 @@
 package com.reuters.cc.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
 
-@Entity (name="USER_DETAILS")
+@Entity
+@Table(name="USER_DETAILS")
 public class UserDetails {
 
-	@Id
+	@Id	@GeneratedValue
 	@Column(name="USER_ID")
 	private int userId;
 	
 	@Column (name="USER_NAME")
 	private String userName;
 	
+	private Date joinedDate;
 	
+	private String Address;
+	
+	@Lob
+	private String description;
+	
+	
+	
+	
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	public int getUserId() {
 		return userId;
 	}

ff4d8082952d3d23dcabb400573d1501efd46e31
kiwichengcheng <kiwichengcheng@163.com>
2014-11-15 16:25:24 +0800

    learn hibernate

--- src/main/java/com/reuters/cc/HibernateTest.java
+++ src/main/java/com/reuters/cc/HibernateTest.java
@@ -7,6 +7,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.reuters.cc.model.Address;
 import com.reuters.cc.model.UserDetails;
 
 public class HibernateTest {
@@ -17,22 +18,33 @@ public class HibernateTest {
 
 		UserDetails user = new UserDetails();
 		user.setUserName("First User");
-		user.setAddress("First User's address");
+		
+		Address address = new Address();
+		address.setStreet("Stree Name");
+		address.setCity("City Name");
+		user.setHomeAddress(address);
 		user.setJoinedDate(new Date());
 		user.setDescription("Description of the user goes here");
 		
-		UserDetails user2 = new UserDetails();
+		
+
+		Address address2 = new Address();
+		address2.setStreet("Office Stree Name");
+		address2.setCity("Office City Name");
+		user.setOfficeAddress(address2);
+		
+		/*UserDetails user2 = new UserDetails();
 		user2.setUserName("Second User");
-		user2.setAddress("First User's address");
+		user2.setAddress();
 		user2.setJoinedDate(new Date());
 		user2.setDescription("Description of the user goes here");
-		
+		*/
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		SessionFactory sessionFactory = context.getBean("sessionFactory",SessionFactory.class);
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(user);
-		session.save(user2);
+		//session.save(user2);
 		session.getTransaction().commit();
 		
 		/*session.close();
--- src/main/java/com/reuters/cc/model/UserDetails.java
+++ src/main/java/com/reuters/cc/model/UserDetails.java
@@ -2,27 +2,44 @@ package com.reuters.cc.model;
 
 import java.util.Date;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name="USER_DETAILS")
 public class UserDetails {
 
-	@Id	@GeneratedValue
+	@Id	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="USER_ID")
 	private int userId;
 	
 	@Column (name="USER_NAME")
 	private String userName;
 	
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date joinedDate;
 	
-	private String Address;
+	@Embedded
+	@AttributeOverrides({
+	@AttributeOverride(name="street",column=@Column(name="HOME_STREET_NAME")),
+	@AttributeOverride(name="city",column=@Column(name="HOME_CITY_NAME")),
+	@AttributeOverride(name="state",column=@Column(name="HOME_STATE_NAME")),
+	@AttributeOverride(name="pincode",column=@Column(name="HOME_PIN_CODE")),
+	})
+	private Address homeAddress;
+	
+	@Embedded
+	private Address officeAddress;
 	
 	@Lob
 	private String description;
@@ -36,11 +53,21 @@ public class UserDetails {
 	public void setJoinedDate(Date joinedDate) {
 		this.joinedDate = joinedDate;
 	}
-	public String getAddress() {
-		return Address;
+
+
+	
+	
+	public Address getHomeAddress() {
+		return homeAddress;
 	}
-	public void setAddress(String address) {
-		Address = address;
+	public void setHomeAddress(Address homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+	public Address getOfficeAddress() {
+		return officeAddress;
+	}
+	public void setOfficeAddress(Address officeAddress) {
+		this.officeAddress = officeAddress;
 	}
 	public String getDescription() {
 		return description;
